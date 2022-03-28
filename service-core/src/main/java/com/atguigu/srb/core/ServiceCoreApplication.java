c819e9ace8bcda7c37bc02ee960f9d3246e84638
Yuran <1037951804@qq.com>
2022-03-28 16:08:04 +0800

    first commit

---
+++ service-core/src/main/java/com/atguigu/srb/core/ServiceCoreApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.srb.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Wang Yuran
+ * @create 2022-03-23 18:29
+ */
+
+@SpringBootApplication
+@ComponentScan({"com.atguigu.srb", "com.atguigu.common"})
+public class ServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCoreApplication.class, args);
+    }
+}
---
+++ service-core/src/main/java/com/atguigu/srb/core/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.atguigu.srb.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Wang Yuran
+ * @create 2022-03-23 18:28
+ */
+@Configuration
+@MapperScan("com.atguigu.srb.core.mapper")
+@EnableTransactionManagement //事务处理
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//分页
+        return interceptor;
+    }
+}
